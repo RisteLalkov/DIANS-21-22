906fe93f6e1d04fb3e536b13b8abd0f54e911155
RisteLalkov <x.lalkov@gmail.com>
2021-12-27 22:44:58 +0100

    my atm first commit

---
+++ my atm/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using my_atm.Areas.Identity.Data;
+using my_atm.Data;
+
+[assembly: HostingStartup(typeof(my_atm.Areas.Identity.IdentityHostingStartup))]
+namespace my_atm.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<my_atmDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("my_atmDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                })
+                    .AddEntityFrameworkStores<my_atmDbContext>();
+            });
+        }
+    }
+}
---
+++ my atm/Migrations/20211226160249_Initial-Create-Again.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace my_atm.Migrations
+{
+    public partial class InitialCreateAgain : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
